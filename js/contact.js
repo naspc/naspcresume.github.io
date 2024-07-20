8c607a32a8a187d936b91b6bb97c920cdd373c4a
naspc <nassirpc@gmail.com>
2024-07-20 12:59:13 -0700

    google sheet first try

---
+++ js/contact.js
@@ -0,0 +1,16 @@
+  const scriptURL = 'https://script.google.com/macros/s/AKfycbz98Zf588UgbglU7GQDfO6IxzTYdNmnyG5FcerGuqVU3yJ8K_YMymbq6FZZ1FRCSagOeg/exec'
+  const form = document.forms['submit-to-google-sheet']
+  const msg = document.getElementById("msg")
+
+  form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+      .then(response => {
+        msg.innerHTML = "Message has been sent successfully"
+        setTimeout(function() {
+            msg.innerHTML = ""
+        },5000)
+        form.reset()
+      })
+      .catch(error => console.error('Error!', error.message))
+  })
