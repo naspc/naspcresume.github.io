c214d1550e1f8affd2f02510d90a16cd2239900a
nassir <57649298+naspc@users.noreply.github.com>
2025-01-25 15:24:34 -0800

    slideshow fix

--- js/resume.js
+++ js/resume.js
@@ -29,29 +29,29 @@
 
  // Dark mode toggle function
  function myFunction() {
-  let element = document.body;
-  element.classList.toggle("dark");
+   let element = document.body;
+   element.classList.toggle("dark");
 
-  // Toggle text color to white when dark mode is active
-  if (element.classList.contains("dark")) {
-    document.body.style.color = 'white';  // Change text color to white in dark mode
-  } else {
-    document.body.style.color = '';  // Revert to default text color
-  }
-}
+   // Toggle text color to white when dark mode is active
+   if (element.classList.contains("dark")) {
+     document.body.style.color = 'white';  // Change text color to white in dark mode
+   } else {
+     document.body.style.color = '';  // Revert to default text color
+   }
+ }
 
-// Slideshow functionality
-var myIndex = 0;
-carousel();
+ // Slideshow functionality
+ var myIndex = 0;
+ carousel();
 
-function carousel() {
-  var i;
-  var x = document.getElementsByClassName("mySlides");
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";  
-  }
-  myIndex++;
-  if (myIndex > x.length) {myIndex = 1}    
-  x[myIndex-1].style.display = "block";  
-  setTimeout(carousel, 2000); // Change image every 2 seconds
-}
+ function carousel() {
+   var i;
+   var x = document.getElementsByClassName("mySlides");
+   for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";  
+   }
+   myIndex++;
+   if (myIndex > x.length) {myIndex = 1}    
+   x[myIndex-1].style.display = "block";  
+   setTimeout(carousel, 2000); // Change image every 2 seconds
+ }
