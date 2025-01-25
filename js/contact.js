effdcddaa419d6213a08fd9cc6a4d1b7ceae5dfd
nassir <57649298+naspc@users.noreply.github.com>
2025-01-25 14:47:45 -0800

    update form js

--- js/contact.js
+++ js/contact.js
@@ -1,18 +1,22 @@
- // Form submission handler
- const scriptURL = 'https://script.google.com/macros/s/AKfycbz98Zf588UgbglU7GQDfO6IxzTYdNmnyG5FcerGuqVU3yJ8K_YMymbq6FZZ1FRCSagOeg/exec'
- const form = document.forms['submit-to-google-sheet']
- const msg = document.getElementById("msg")
+const scriptURL = 'https://script.google.com/macros/s/AKfycbz98Zf588UgbglU7GQDfO6IxzTYdNmnyG5FcerGuqVU3yJ8K_YMymbq6FZZ1FRCSagOeg/exec';
+const form = document.forms['submit-to-google-sheet'];  // Ensure this matches your form's name
+const msg = document.getElementById("msg");
 
- form.addEventListener('submit', e => {
-   e.preventDefault()
-   fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-     .then(response => {
-       msg.innerHTML = "Message has been sent successfully"
-       setTimeout(function() {
-           msg.innerHTML = ""
-       }, 5000)
-       form.reset()
-     })
-     .catch(error => console.error('Error!', error.message))
- })
-
+form.addEventListener('submit', e => {
+  e.preventDefault();  // Prevent the form from actually submitting
+  fetch(scriptURL, {
+    method: 'POST',
+    body: new FormData(form)
+  })
+  .then(response => {
+    msg.innerHTML = "Message has been sent successfully!";
+    setTimeout(function() {
+      msg.innerHTML = "";
+    }, 5000);
+    form.reset();  // Reset the form
+  })
+  .catch(error => {
+    console.error('Error!', error.message);
+    msg.innerHTML = "Error! Unable to send message.";
+  });
+});
