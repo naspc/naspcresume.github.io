8adc2ee3ef1037dc3fd154632997a883fb4921bd
nassir <57649298+naspc@users.noreply.github.com>
2025-01-25 15:22:32 -0800

    reverse, scratch contact sheet

--- js/contact.js
+++ js/contact.js
@@ -2,11 +2,8 @@ const scriptURL = 'https://script.google.com/macros/s/AKfycbz98Zf588UgbglU7GQDfO
 const form = document.forms['submit-to-google-sheet'];  // Ensure this matches your form's name
 const msg = document.getElementById("msg");
 
-function submitForm(event) {
-  event.preventDefault(); // Prevent the form from actually submitting
-
-  msg.innerHTML = "Sending..."; // Show loading message while submitting
-
+form.addEventListener('submit', e => {
+  e.preventDefault();  // Prevent the form from actually submitting
   fetch(scriptURL, {
     method: 'POST',
     body: new FormData(form)
@@ -15,11 +12,11 @@ function submitForm(event) {
     msg.innerHTML = "Message has been sent successfully!";
     setTimeout(function() {
       msg.innerHTML = "";
-    }, 5000); // Clear the success message after 5 seconds
-    form.reset(); // Reset the form after submission
+    }, 5000);
+    form.reset();  // Reset the form
   })
   .catch(error => {
     console.error('Error!', error.message);
-    msg.innerHTML = "Error! Unable to send message."; // Show error message
+    msg.innerHTML = "Error! Unable to send message.";
   });
-}
+});
